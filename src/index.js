5d5832ed73137ac754988b50e7bb1e32cae6bda7
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-02 18:54:54 -0400

    setup react-router-dom and Context Provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PortfolioContextProvider } from './Context/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <PortfolioContextProvider>
+        <App />
+      </PortfolioContextProvider>
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
