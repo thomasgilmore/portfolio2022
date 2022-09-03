8a36e8f7a80cea6bea440c8aabeac5931610551d
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-03 10:30:30 -0400

    imported NavBar into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
 import Home from './Components/Home';
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Routes>
         <Route path='*' element={<Home />} /> 
       </Routes>
