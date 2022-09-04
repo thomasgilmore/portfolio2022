18b77990ac5ae62cca3daf9cc01a2b9a813f3713
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-04 17:32:56 -0400

    handle dark and light mode

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,11 +1,18 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 const PortfolioContext = createContext();
 
 const PortfolioContextProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const handleThemeChange = () => {
+     setTheme(theme === 'light' ? 'dark' : 'light')
+  }
 
   const value = {
-
+    theme, 
+    setTheme,
+    handleThemeChange
   }
   return (
     <PortfolioContext.Provider value={value} >
