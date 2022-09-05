f7da4cd1312be7f779a67e19cecaf58cb72f8f85
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-04 21:10:35 -0400

    created light and dark mode button

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PortfolioContext } from '../Context/Context';
+import { BsSun } from 'react-icons/bs';
+import { FiMoon } from 'react-icons/fi';
 import './navbar.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 export default function NavBar() {
+  const { theme, handleThemeChange } = useContext(PortfolioContext)
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg={theme} variant={theme} expand="lg">
       <Container>
         <Navbar.Brand href="#home">TG</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -14,7 +18,9 @@ export default function NavBar() {
           <Nav className="navItems">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
+            <button onClick={handleThemeChange} className={`${theme}Button`}>{theme === 'light' ? <BsSun /> : <FiMoon />}</button>
           </Nav>
         </Navbar.Collapse>
       </Container>
