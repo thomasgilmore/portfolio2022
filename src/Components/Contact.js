4bd119e2f0fb7b0ab43e924a34745f828baa3be8
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-05 16:11:34 -0400

    validate contact form info

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,16 +8,16 @@ export default function Contact() {
     <form className='contactForm' id="contact">
       <h2>Contact</h2>
       <label>Name
-        <input type="text" name="Name" placeholder='Name' onChange={handeContactNameChange} />
+        <input type="text" name="Name" placeholder='Name' onChange={handeContactNameChange} required />
       </label>
       <label>Email Address
-        <input type="email" name="Email" placeholder='Email Address' onChange={handeContactEmailChange} />
+        <input type="email" name="Email" placeholder='Email Address' onChange={handeContactEmailChange} pattern = "^([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3})|(\d{3}-\d{3}-\d{4})$" required />
       </label>
       <label>Phone Number
         <input type="text" name="Phone Number" placeholder='Phone Number' onChange={handeContactPhoneNumberChange} />
       </label>
       <label>Message
-        <textarea onChange={handeContactMessageChange} />
+        <textarea onChange={handeContactMessageChange} required />
       </label>
       <button className={`${theme}ContactSubmitButton`}>Submit</button>
     </form>
