9a26b6256e729a44cdd3a00205ecc5ae732dac27
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-04 21:12:39 -0400

    working on Home.js:

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Container from 'react-bootstrap/Container';
+import { PortfolioContext } from '../Context/Context';
 import './home.css';
 
 export default function Home() {
+  const { theme } = useContext(PortfolioContext);
   return (
-    <div></div>
+    <Container>
+      <div className='portfolioHero'>
+        <h1>Hello, I'm Thomas Gilmore</h1>
+        <h1>I'm a Front End Developer</h1>
+        <div className='portfolioHeroSocialIcons'>
+        
+        </div>
+      </div>
+    </Container>
   )
 }
