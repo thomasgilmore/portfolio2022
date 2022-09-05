5a6d65ce1ca27df3c3ad8bb5a7951c9bef1d7929
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-05 13:41:15 -0400

    added line to about.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,6 +7,7 @@ export default function About() {
       <h2 id="about">About</h2>
       <p>I'm a self taught Front End Developer. I've taken multiple course online including the Front End Developer Nano Degree program from Udacity, The Complete React Developer Course, and many others as well. These courses have helped me developer many different front end projects from websites to web applications using React.</p>
       <p>These courses have also help me become a Tutor for a Full Stack Flex Web Development Coding Bootcamp where I help students from all over the world with debugging, conceptualizing, and completing assignments remotely in full stack web development. Topics range from HTML, CSS, JavaScript, React, and Node.js.</p>
+      <p>On my free time I enjoy playing kickball in this league I'm part of.</p>
     </div>
   )
 }
