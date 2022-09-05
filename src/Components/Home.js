7b72e41d6e3d4c069bae5b8725528107d64691f4
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-05 13:15:46 -0400

    resume button now opens resume

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,9 +6,10 @@ import GitHubMark from '../Assets/GitHub-Mark/PNG/GitHub-Mark-120px-plus.png';
 import GitHubMarkLight from '../Assets/GitHub-Mark/PNG/GitHub-Mark-Light-120px-plus.png';
 import LinkedinLogo from '../Assets/iconfinder_1_Linkedin_unofficial_colored_svg_5296501.png';
 import About from './About';
+import Contact from './Contact';
 
 export default function Home() {
-  const { theme } = useContext(PortfolioContext);
+  const { theme, handleDownloadResume } = useContext(PortfolioContext);
   return (
     <Container>
       <div className='portfolioHero'>
@@ -16,11 +17,12 @@ export default function Home() {
         <h1>I'm a Front End Developer</h1>
         <div className='portfolioHeroSocialIcons'>
           <a href="https://github.com/thomasgilmore95" target="__blank">{theme === 'light' ? <img src={GitHubMark} className="gitHubLogo" alt="GitHub Logo Link to My GitHub Account" /> : <img src={GitHubMarkLight}   className="gitHubLogo" alt="GitHub Logo Link to My HitHub Account" />}</a>
-          <button>Resume</button>
+          <button onClick={handleDownloadResume} className="">Resume</button>
           <a href="https://www.linkedin.com/in/thomasgilmore95/" target="__blank"><img src={LinkedinLogo} className="linkedinLogo" alt="Linkedin Logo to Linkedin Account" /></a>
         </div>
       </div>
       <About />
+      <Contact />
     </Container>
   )
 }
