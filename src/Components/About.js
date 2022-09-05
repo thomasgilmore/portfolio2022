d999879e998daf37b88feb97ebf1103261702da6
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-05 16:27:46 -0400

    added more to the about.js file

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,9 +5,11 @@ export default function About() {
   return (
     <div className='aboutDiv'>
       <h2 id="about">About</h2>
-      <p>I'm a self taught Front End Developer. I've taken multiple course online including the Front End Developer Nano Degree program from Udacity, The Complete React Developer Course, and many others as well. These courses have helped me developer many different front end projects from websites to web applications using React.</p>
-      <p>These courses have also help me become a Tutor for a Full Stack Flex Web Development Coding Bootcamp where I help students from all over the world with debugging, conceptualizing, and completing assignments remotely in full stack web development. Topics range from HTML, CSS, JavaScript, React, and Node.js.</p>
-      <p>On my free time I enjoy playing kickball in this league I'm part of.</p>
+      <p>I'm a self taught Front End Developer. I've taken multiple course online including the Front End Developer Nano Degree program from Udacity, The Complete React Developer Course, and many others as well. These courses have helped me develop many different front end projects from websites to web applications.</p>
+      <p>These courses have also help me become a tutor for a Full Stack Flex Web Development Coding Bootcamp where I help students from all over the world with debugging, conceptualizing, and completing assignments remotely in full stack web development. Topics range from HTML, CSS, JavaScript, React, and Node.js. Tutoring is also a great learning experice because you never know what a student may ask or need help with.</p>
+      <p>I always have some project in the works to keep my self busy. I've worked with Senior Developer and I alwas try to make it a learning experice for myself. Where I either learn something new, get better at something or even see how to do something in a different way.</p>
+      <p>What I've learned from being self taught developer so far is that there is no one way to do everything, there is alwasys another way.</p>
+      <p>On my free time I enjoy hanging out with friends and playing kickball in this league I'm part of.</p>
     </div>
   )
 }
