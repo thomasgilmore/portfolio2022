7fe433e071b0962abf26612f2738ec9599e34150
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-04 21:13:09 -0400

    added imports to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-import React from 'react';
+import './darkMode.css';
+import './lightMode.css';
+import React, { useContext } from 'react';
+import { PortfolioContext } from './Context/Context';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import Home from './Components/Home';
 
 function App() {
+  const { theme } = useContext(PortfolioContext);
   return (
-    <div>
-      <NavBar />
-      <Routes>
-        <Route path='*' element={<Home />} /> 
-      </Routes>
-    </div>
+      <div className={`${theme}`}>
+        <NavBar />
+        <Routes>
+          <Route path='*' element={<Home />} /> 
+        </Routes>
+      </div>
   );
 }
 
