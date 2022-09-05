4b0a2c57ccb750f10a65e212209613fc2e588d57
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-04 21:08:57 -0400

    created About.js

---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <p>I'm a self taught Front End Developer. I've taken multiple course online including the Front End Developer Nano Degree program from Udacity, The Complete React Developer Course, and many others as well. These courses have helped me developer many different front end projects from websites to web applications using React.</p>
+      <p>These courses have also help me become a Tutor for a Full Stack Flex Web Development Coding Bootcamp where I help students from all over the world with debugging, conceptualizing, and completing assignments remotely in full stack web development. Topics range from HTML, CSS, JavaScript, React, and Node.js.</p>
+    </div>
+  )
+}
