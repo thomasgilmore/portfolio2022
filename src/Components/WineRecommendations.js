528b15c66814d760c7c1ef151543dc5e035e75a8
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-05 22:45:04 -0400

    working on Wine Recommendations

---
+++ src/Components/WineRecommendations.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './winerecommendations.css';
+import Container from 'react-bootstrap/Container';
+
+export default function WineRecommendations() {
+  return (
+    <div className='wineRecommendationsDiv'>
+      <Container>
+      <h1>Wine Recommendations</h1>
+      </Container>
+    </div>
+  )
+}
