3a9d594e6eef7ea1ee55f26be0e9463aa2db6e7e
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-05 23:19:34 -0400

    working on ProjectCard styling and dark and light mode

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PortfolioContext } from '../Context/Context';
 import './projectcard.css';
 import { Link } from 'react-router-dom';
 
 export default function ProjectCard(props) {
+  const { theme } = useContext(PortfolioContext);
   return(
-    <Link to={props.link}>
+    <Link to={props.link} className="projectCardLink">
       <div className='projectCardDiv'>
-        <img src={props.img} alt={props.name} className="projectCardImg" />
-        <h2>{props.title}</h2>
-        <p>View Project</p>
+        <div className='projectCardImgDiv'>
+          <img src={props.img} alt={props.name} className="projectCardImg" />
+        </div>
+        <h2 className={`projectCardTitle ${theme}projectCardTitle`}>{props.title}</h2>
+        <p className={`projectCardViewProject ${theme}projectCardViewProject`}>View Project</p>
       </div>
     </Link>
   )
