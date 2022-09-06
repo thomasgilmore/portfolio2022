892a703feddee0d964278b2a690eed8173c76a86
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-06 00:16:23 -0400

    adjusted styling on project cards

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,9 +8,11 @@ export default function Projects() {
   return (
     <div className='projectsDiv' id="projects">
       <h2>Projects</h2>
-      {projects.map((project) => {
-        return <ProjectCard key={project.title} img={project.img} title={project.title} url={project.url} link={project.link} />
-      })}
+      <div className='projectsListDiv'>
+        {projects.map((project) => {
+          return <ProjectCard key={project.title} img={project.img} title={project.title} url={project.url} link={project.link} />
+        })}
+      </div>
     </div>
   )
 }
