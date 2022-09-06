97935136110d012749cd6d35318aaa45e7ecea70
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-06 16:47:00 -0400

    created DesignoMultiPageWebsite.js

---
+++ src/Components/DesignoMultiPageWebsite.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './designomultipagewebsite.css';
+import Container from 'react-bootstrap/Container';
+import ProjectNav from './ProjectNav';
+
+export default function DesignoMultiPageWebsite() {
+  return(
+    <div>
+      <Container>
+        <ProjectNav title='Designo Multi-Page Website' />
+      </Container>
+    </div>
+  )
+}
