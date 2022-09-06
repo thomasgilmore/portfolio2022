97d2ae8bb5ba27815fb28e7c25ea7fca7384ac75
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-05 21:46:10 -0400

    Projects.js

---
+++ src/Components/Projects.js
@@ -0,0 +1,16 @@
+import './projects.css';
+import React, { useContext } from 'react';
+import { PortfolioContext } from '../Context/Context';
+import ProjectCard from './ProjectCard';
+
+export default function Projects() {
+  const { projects } = useContext(PortfolioContext);
+  return (
+    <div className='projectsDiv' id="projects">
+      <h2>Projects</h2>
+      {projects.map((project) => {
+        return <ProjectCard key={project.title} img={project.img} title={project.title} url={project.url} link={project.link} />
+      })}
+    </div>
+  )
+}
