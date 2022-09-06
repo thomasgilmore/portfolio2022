ce2f479b0ca5ecd9dd9e585ded1e46fc3f20d050
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-06 16:47:50 -0400

    created new routes for Pokemon Index and Designo Multi Page Website

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import './lightMode.css';
 import React, { useContext } from 'react';
 import { PortfolioContext } from './Context/Context';
 import { Routes, Route } from 'react-router-dom';
-import NavBar from './Components/NavBar';
 import Home from './Components/Home';
 import WineRecommendations from './Components/WineRecommendations';
 import MoodyCreatureSociety from './Components/MoodyCreatureSociety';
+import PokemonIndex from './Components/PokemonIndex';
+import DesignoMultiPageWebsite from './Components/DesignoMultiPageWebsite';
 
 function App() {
   const { theme } = useContext(PortfolioContext);
   return (
       <div className={`${theme}`}>
-        <NavBar />
         <Routes>
           <Route path='*' element={<Home />} />
           <Route path='/wine-recommendations' element={<WineRecommendations />} />
           <Route path="/moody-creature-society" element={<MoodyCreatureSociety />} />
+          <Route path="/pokemon-index" element={<PokemonIndex />} />
+          <Route path="/designo-multi-page-website" element={<DesignoMultiPageWebsite />} />
         </Routes>
       </div>
   );
