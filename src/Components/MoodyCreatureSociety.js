aed086a8c53144d73d27a81c0ccd488f3edfa83d
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-06 00:14:51 -0400

    Mood Creature Society

---
+++ src/Components/MoodyCreatureSociety.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './moodycreaturesociety.css';
+import Container from 'react-bootstrap/Container';
+
+export default function MoodyCreatureSociety() {
+  return(
+    <div>
+      <Container>
+        <h1>Moody Creature Society</h1>
+      </Container>
+    </div>
+  )
+}
