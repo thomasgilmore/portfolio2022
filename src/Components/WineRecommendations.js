c1b90d89527a890d9f7f28778923fd4871366b02
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-07 09:57:09 -0400

    started to add text to WineRecommendations.js

--- src/Components/WineRecommendations.js
+++ src/Components/WineRecommendations.js
@@ -7,7 +7,10 @@ export default function WineRecommendations() {
   return (
     <div className='wineRecommendationsDiv'>
       <Container>
-      <ProjectNav title="Wine Recommendations" />
+        <ProjectNav title="Wine Recommendations" />
+        <p>I created a Wine Recommendations Application where you can either find a dish that goes well with a given wine (ex. Malbec, Pinot Grigio, Rose Wine, ... etc.). Also you can find a wine that goes well with a food. Food can be a dish name ("steak"), an ingredient name ("salmon"), or a cuisine ("Italian").</p>
+        <p>For the Wine Recommendations I'm using two API endpoints from Spoonacular API. I'm using the get dish pairing for wine were you find a dish that goes well with a given wine and I'm also using get wine pairing that finds a wine that goes well with food.</p>
+        <p>I used React to create the frontend for this project. I used two AJAX calls for the two different API endpoints to return the data to the user and using Netlify to host the app.</p>
       </Container>
     </div>
   )
