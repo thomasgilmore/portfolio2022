83e4167cae558e21547615124da412b1bcce0817
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-07 09:58:09 -0400

    started to work on text on PokemonIdex.js

--- src/Components/PokemonIndex.js
+++ src/Components/PokemonIndex.js
@@ -5,9 +5,11 @@ import ProjectNav from './ProjectNav';
 
 export default function PokemonIndex() {
   return(
-    <div>
+    <div className='pokemonIndexDiv'>
       <Container>
         <ProjectNav title="Pokémon Index" />
+        <p>Implements user login feature with Google OAuth and utilizes context hook to handle UI states</p>
+        <p>Integrates REST protocol that works with Pokemoncg.io API endpoints</p>
       </Container>
     </div>
   )
