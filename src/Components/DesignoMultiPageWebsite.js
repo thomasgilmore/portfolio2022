11165b165752f1bf652cb486de5403c80f185493
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-08 15:11:23 -0400

    added some text to Designo Project

--- src/Components/DesignoMultiPageWebsite.js
+++ src/Components/DesignoMultiPageWebsite.js
@@ -8,6 +8,7 @@ export default function DesignoMultiPageWebsite() {
     <div className='designoMultiPageWebsiteDiv'>
       <Container>
         <ProjectNav title='Designo Multi-Page Website' />
+        <p>This project I followed a figma design that was given to me to create a multi-page react website. To create the different pages I used React Router to display the different components for each page.</p>
       </Container>
     </div>
   )
