98f3e0ba4b078199a541940f38b15353f7fdbb9b
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-11 20:59:28 -0400

    typewriter-effect added to Home.js and adjusted styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import { PortfolioContext } from '../Context/Context';
 import './home.css';
 import NavBar from './NavBar';
+import Typewriter from 'typewriter-effect';
 import GitHubMark from '../Assets/GitHub-Mark/PNG/GitHub-Mark-120px-plus.png';
 import GitHubMarkLight from '../Assets/GitHub-Mark/PNG/GitHub-Mark-Light-120px-plus.png';
 import LinkedinLogo from '../Assets/iconfinder_1_Linkedin_unofficial_colored_svg_5296501.png';
@@ -16,8 +17,13 @@ export default function Home() {
     <Container>
       <NavBar />
       <div className='portfolioHero'>
-        <h1>Hi there, I'm Thomas.</h1>
-        <h1>Front End Developer.</h1>
+        <h1 className='hiThere'>Hi there, I'm Thomas.</h1>
+        <Typewriter options={{
+          strings: ['Front End Developer.', 'Self Taught Developer.'],
+          autoStart: true,
+          loop: true,
+          cursorClassName: 'heroCursor',
+        }} />
         <div className='portfolioHeroSocialIcons'>
           <a href="https://github.com/thomasgilmore95" target="__blank">{theme === 'light' ? <img src={GitHubMark} className="gitHubLogo" alt="GitHub Logo Link to My GitHub Account" /> : <img src={GitHubMarkLight}   className="gitHubLogo" alt="GitHub Logo Link to My HitHub Account" />}</a>
           <button onClick={handleDownloadResume} className={`${theme}ResumeButton`}>Resume</button>
