fb73baad77e83ffdf21cd09b7958b284595412ab
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-12 10:35:50 -0400

    working on text for projects

--- src/Components/MoodyCreatureSociety.js
+++ src/Components/MoodyCreatureSociety.js
@@ -10,9 +10,7 @@ export default function MoodyCreatureSociety() {
         <ProjectNav title="Moody Creature Society" />
         <p>This project I worked with a team lead, backend developer, and the client. I worked on the Frontend landing page using React for a NFT. It displays price info, date release, some of the pictures of the NFTs for sale, etc.</p> 
 
-        <p>I was given all of the elements for the website (ex. images and text) and they gave me a sketch to follow that was written on a piece of paper. It was definitely one of the more interesting ways to get a design and possibly made it more difficult. However, I’m always up for a good challenge to work on. 
-        
-        </p>
+        <p>I was given all of the elements for the website (ex. images and text) and they gave me a sketch to follow that was written on a piece of paper. It was definitely one of the more interesting ways to get a design and possibly made it more difficult. However, I'm always up for a good challenge to work on.</p>
       </Container>
     </div>
   )
