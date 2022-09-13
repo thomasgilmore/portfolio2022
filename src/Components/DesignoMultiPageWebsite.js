43eeb2168a15da6d570448e4f477143a75fd5d98
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-13 10:11:43 -0400

    adjuested class name for project text

--- src/Components/DesignoMultiPageWebsite.js
+++ src/Components/DesignoMultiPageWebsite.js
@@ -8,7 +8,7 @@ export default function DesignoMultiPageWebsite() {
     <div className='designoMultiPageWebsiteDiv'>
       <Container>
         <ProjectNav title='Designo Multi-Page Website' />
-        <p>This project I followed a figma design that was given to me to create a multi-page react website. To create the different pages I used React Router to display the different components for each page.</p>
+        <p className="projectText">This project I followed a figma design that was given to me to create a multi-page react website. To create the different pages I used React Router to display the different components for each page.</p>
       </Container>
     </div>
   )
--- src/Components/PokemonIndex.js
+++ src/Components/PokemonIndex.js
@@ -8,12 +8,12 @@ export default function PokemonIndex() {
     <div className='pokemonIndexDiv'>
       <Container>
         <ProjectNav title="Pokémon Index" />
-        <p>Implements user login feature with Google OAuth and utilizes context hook to handle UI states</p>
-        <p>Integrates REST protocol that works with Pokemoncg.io API endpoints</p>
+        <p className="projectText">Implements user login feature with Google OAuth and utilizes context hook to handle UI states</p>
+        <p className="projectText">Integrates REST protocol that works with Pokemoncg.io API endpoints</p>
 
-        <p>I like to add new coding challenges for myself while doing new projects. So I can learn something new while creating a new project. I implemented user login feature with Google Oauth and the UI updates to display the users picture and name in the nav when they are signed in.</p>
+        <p className="projectText">I like to add new coding challenges for myself while doing new projects. So I can learn something new while creating a new project. I implemented user login feature with Google Oauth and the UI updates to display the users picture and name in the nav when they are signed in.</p>
 
-        <p>Integrate REST protocol that works with Pokemoncg.io API endpoints to fetch the Pokémon cards to display them and you can also search for a particular Pokémon card and the UI will update to only display that Pokémon. I utilized context hook to handle UI states for the application and also created UI components with functional components and hooks. 
+        <p className="projectText">Integrate REST protocol that works with Pokemoncg.io API endpoints to fetch the Pokémon cards to display them and you can also search for a particular Pokémon card and the UI will update to only display that Pokémon. I utilized context hook to handle UI states for the application and also created UI components with functional components and hooks. 
         </p>
       </Container>
     </div>
