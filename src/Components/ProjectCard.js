b7b510ccf2cc7036981e1a303523f459535a27c1
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-16 11:37:34 -0400

    adjusted project card motion and styling

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -7,17 +7,17 @@ import { Link } from 'react-router-dom';
 export default function ProjectCard(props) {
   const { theme } = useContext(PortfolioContext);
   return(
+    <motion.div whileHover={{ scale: 1.03 }} className={`${theme}CardMotionDiv`}>
     <Link to={props.link} className="projectCardLink">
       <div className='projectCardDiv'>
         <div className='projectCardImgDiv'>
-        <motion.div whileHover={{ scale: 1.2 }}>
           <img src={props.img} alt={props.name} 
           className="projectCardImg" />
-          </motion.div>
         </div>
         <h2 className={`projectCardTitle ${theme}projectCardTitle`}>{props.title}</h2>
         <p className={`projectCardViewProject ${theme}projectCardViewProject`}>View Project</p>
       </div>
     </Link>
+    </motion.div>
   )
 }
