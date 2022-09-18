2630ab7186899697273f4b934c41c1ea553647a4
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-18 16:28:51 -0400

    imported contact form alert message into contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,7 @@
+import './contact.css';
 import React, { useContext } from 'react';
 import { PortfolioContext } from '../Context/Context';
-import './contact.css';
+import ContactFormConfirmation from './ContactFormConfirmation';
 
 export default function Contact() {
   const { theme, handeContactNameChange, handeContactEmailChange, handeContactPhoneNumberChange, handeContactMessageChange, handleSubmit } = useContext(PortfolioContext);
@@ -14,11 +15,12 @@ export default function Contact() {
         <input type="email" className={`${theme}ContactFormInput`} name="Email" placeholder='Email Address' onChange={handeContactEmailChange} pattern = "^([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3})|(\d{3}-\d{3}-\d{4})$" required />
       </label>
       <label>Phone Number
-        <input type="text" className={`${theme}ContactFormInput`} name="Phone Number" placeholder='Phone Number' onChange={handeContactPhoneNumberChange} />
+        <input type="tel" className={`${theme}ContactFormInput`} name="Phone Number" placeholder='Phone Number' onChange={handeContactPhoneNumberChange} required />
       </label>
       <label>Message
         <textarea className={`${theme}ContactFormTextArea`} onChange={handeContactMessageChange} required />
       </label>
+      <ContactFormConfirmation confirmationText="Test"/>
       <button className={`${theme}ContactSubmitButton`}>Submit</button>
     </form>
   )
