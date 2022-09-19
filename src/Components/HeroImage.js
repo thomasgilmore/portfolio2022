61a8647e86e56389a1774a05c4f2020585aebeeb
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-19 16:45:17 -0400

    added className

--- src/Components/HeroImage.js
+++ src/Components/HeroImage.js
@@ -12,10 +12,10 @@ import programmerImageLargeWebP from '../Assets/Images/programmer-1653351.webp'
 export default function HeroImage() {
   return (
     <div className='heroImageDiv'>
-      <picture src={programmerImageSmall440} className="heroImage"  alt="Person Coding" >
+      <picture src={programmerImageSmall440} alt="Person Coding" >
         <source srcSet={`${programmerImageSmall440} 440w, ${programmerImageSmall} 640w, ${programmerImageMedium} 1280w, ${programmerImageLarge} 2000w`} />
         <source srcSet={`${programmerImageSmall440WebP} 440w, ${programmerImageSmallWebP} 640w, ${programmerImageMediumWebP} 1280w, ${programmerImageLargeWebP} 2000w`} type='image/webp' />
-        <img src={programmerImageSmall440} alt="Person Coding"/>
+        <img src={programmerImageSmall440} className="heroImage" alt="Person Coding"/>
       </picture>
     </div>
   )
