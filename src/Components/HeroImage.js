0edc3121878a0eabdfc434e45807e2eaa5042e06
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-19 16:13:01 -0400

    added smaller image for mobile

--- src/Components/HeroImage.js
+++ src/Components/HeroImage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './heroimage.css';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import programmerImageSmall440 from '../Assets/Images/programmer-1653351_440.png';
 import programmerImageSmall from '../Assets/Images/programmer-1653351_640.png';
 import programmerImageMedium from '../Assets/Images/programmer-1653351_1280.png';
 import programmerImageLarge from '../Assets/Images/programmer-1653351.png';
@@ -8,7 +9,7 @@ import programmerImageLarge from '../Assets/Images/programmer-1653351.png';
 export default function HeroImage() {
   return (
     <div className='heroImageDiv'>
-      <LazyLoadImage src={programmerImageSmall} className="heroImage" srcSet={`${programmerImageSmall} 640w, ${programmerImageMedium} 1280w, ${programmerImageLarge} 2000w`} alt="Person Coding" />
+      <LazyLoadImage src={programmerImageSmall440} className="heroImage" srcSet={`${programmerImageSmall440} 440w, ${programmerImageSmall} 640w, ${programmerImageMedium} 1280w, ${programmerImageLarge} 2000w`} alt="Person Coding" />
     </div>
   )
 }
