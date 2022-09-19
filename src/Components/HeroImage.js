1ebdd3e5e5b2e69f14ddd061c9104d811a47cef4
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-19 16:42:00 -0400

    added web p for image

--- src/Components/HeroImage.js
+++ src/Components/HeroImage.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './heroimage.css';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 import programmerImageSmall440 from '../Assets/Images/programmer-1653351_440.png';
 import programmerImageSmall from '../Assets/Images/programmer-1653351_640.png';
 import programmerImageMedium from '../Assets/Images/programmer-1653351_1280.png';
 import programmerImageLarge from '../Assets/Images/programmer-1653351.png';
+import programmerImageSmall440WebP from '../Assets/Images/programmer-1653351_440.webp';
+import programmerImageSmallWebP from '../Assets/Images/programmer-1653351_640.webp';
+import programmerImageMediumWebP from '../Assets/Images/programmer-1653351_1280.webp';
+import programmerImageLargeWebP from '../Assets/Images/programmer-1653351.webp'
 
 export default function HeroImage() {
   return (
     <div className='heroImageDiv'>
-      <LazyLoadImage src={programmerImageSmall440} className="heroImage" srcSet={`${programmerImageSmall440} 440w, ${programmerImageSmall} 640w, ${programmerImageMedium} 1280w, ${programmerImageLarge} 2000w`} alt="Person Coding" />
+      <picture src={programmerImageSmall440} className="heroImage"  alt="Person Coding" >
+        <source srcSet={`${programmerImageSmall440} 440w, ${programmerImageSmall} 640w, ${programmerImageMedium} 1280w, ${programmerImageLarge} 2000w`} />
+        <source srcSet={`${programmerImageSmall440WebP} 440w, ${programmerImageSmallWebP} 640w, ${programmerImageMediumWebP} 1280w, ${programmerImageLargeWebP} 2000w`} type='image/webp' />
+        <img src={programmerImageSmall440} alt="Person Coding"/>
+      </picture>
     </div>
   )
 }
