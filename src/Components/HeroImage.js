363cc514ef31f35dfe5a25904ee5729a33a08008
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-19 22:19:40 -0400

    picture sizes

--- src/Components/HeroImage.js
+++ src/Components/HeroImage.js
@@ -13,8 +13,8 @@ export default function HeroImage() {
   return (
     <div className='heroImageDiv'>
       <picture src={programmerImageSmall440} alt="Person Coding" >
-        <source srcSet={`${programmerImageSmall440WebP} 440w, ${programmerImageSmallWebP} 640w, ${programmerImageMediumWebP} 1280w, ${programmerImageLargeWebP} 2000w`} type='image/webp' />
-        <source srcSet={`${programmerImageSmall440} 440w, ${programmerImageSmall} 640w, ${programmerImageMedium} 1280w, ${programmerImageLarge} 2000w`} type='image/png' />
+        <source srcSet={`${programmerImageSmall440WebP} 440w, ${programmerImageSmallWebP} 640w, ${programmerImageMediumWebP} 1280w, ${programmerImageLargeWebP} 2000w`} sizes="(max-width: 440px) 440px, (max-width: 640px) 640px, (max-width: 1280px) 1280px, 2000px" type='image/webp' />
+        <source srcSet={`${programmerImageSmall440} 440w, ${programmerImageSmall} 640w, ${programmerImageMedium} 1280w, ${programmerImageLarge} 2000w`} sizes="(max-width: 440px) 440px, (max-width: 640px) 640px, (max-width: 1280px) 1280px, 2000px" type='image/png' />
         <img src={programmerImageSmall440WebP} className="heroImage" alt="Person Coding"/>
       </picture>
     </div>
