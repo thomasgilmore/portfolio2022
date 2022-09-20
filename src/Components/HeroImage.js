6a04ff1f41924699bba4468dfe20b3fcbcac11a0
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-19 23:48:42 -0400

    help?

--- src/Components/HeroImage.js
+++ src/Components/HeroImage.js
@@ -19,10 +19,10 @@ import programmerImageLargeWebP from '../Assets/Images/programmer-1653351.webp'
 export default function HeroImage() {
   return (
     <div className='heroImageDiv'>
-      <picture src={programmerImageSmall440} alt="Person Coding" >
+      <picture>
         <source srcSet={`${programmerImage200WebP} 200w, ${programmerImage300WebP} 300w, ${programmerImage390WebP} 390w, ${programmerImageSmall440WebP} 440w, ${programmerImageSmallWebP} 640w, ${programmerImageMediumWebP} 1280w, ${programmerImageLargeWebP} 2000w`} sizes="(max-width: 200px) 200px, (max-width: 300px) 300px, (max-width: 390px) 390px, (max-width: 440px) 440px, (max-width: 640px) 640px, (max-width: 1280px) 1280px, 2000px" type='image/webp' />
         <source srcSet={`${programmerImage200} 200w, ${programmerImage300} 300w, ${programmerImage390} 390w, ${programmerImageSmall440} 440w, ${programmerImageSmall} 640w, ${programmerImageMedium} 1280w, ${programmerImageLarge} 2000w`} sizes="(max-width: 200px) 200px, (max-width: 300px) 300px, (max-width: 390px) 390px, (max-width: 440px) 440px, (max-width: 640px) 640px, (max-width: 1280px) 1280px, 2000px" type='image/png' />
-        <LazyLoadImage src={programmerImageSmall440WebP} className="heroImage" alt="Person Coding"/>
+        <LazyLoadImage src={programmerImage200} className="heroImage" alt="Person Coding"/>
       </picture>
     </div>
   )
