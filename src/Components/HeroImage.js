93494cc044f015c4b30e31a759bf96be56cb23aa
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-22 22:22:00 -0400

    Image CDN test 2

--- src/Components/HeroImage.js
+++ src/Components/HeroImage.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './heroimage.css';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import programmerImage200 from '../Assets/Images/programmer-1653351_200.png';
-import programmerImage300 from '../Assets/Images/programmer-1653351_300.png';
-import programmerImage390 from '../Assets/Images/programmer-1653351_390.png';
-import programmerImageSmall440 from '../Assets/Images/programmer-1653351_440.png';
-import programmerImageSmall from '../Assets/Images/programmer-1653351_640.png';
-import programmerImageMedium from '../Assets/Images/programmer-1653351_1280.png';
-import programmerImageLarge from '../Assets/Images/programmer-1653351.png';
-import programmerImage200WebP from '../Assets/Images/programmer-1653351_200.webp';
-import programmerImage300WebP from '../Assets/Images/programmer-1653351_300.webp';
-import programmerImage390WebP from '../Assets/Images/programmer-1653351_390.webp';
-import programmerImageSmall440WebP from '../Assets/Images/programmer-1653351_440.webp';
-import programmerImageSmallWebP from '../Assets/Images/programmer-1653351_640.webp';
-import programmerImageMediumWebP from '../Assets/Images/programmer-1653351_1280.webp';
-import programmerImageLargeWebP from '../Assets/Images/programmer-1653351.webp'
+// import programmerImage200 from '../Assets/Images/programmer-1653351_200.png';
+// import programmerImage300 from '../Assets/Images/programmer-1653351_300.png';
+// import programmerImage390 from '../Assets/Images/programmer-1653351_390.png';
+// import programmerImageSmall440 from '../Assets/Images/programmer-1653351_440.png';
+// import programmerImageSmall from '../Assets/Images/programmer-1653351_640.png';
+// import programmerImageMedium from '../Assets/Images/programmer-1653351_1280.png';
+// import programmerImageLarge from '../Assets/Images/programmer-1653351.png';
+// import programmerImage200WebP from '../Assets/Images/programmer-1653351_200.webp';
+// import programmerImage300WebP from '../Assets/Images/programmer-1653351_300.webp';
+// import programmerImage390WebP from '../Assets/Images/programmer-1653351_390.webp';
+// import programmerImageSmall440WebP from '../Assets/Images/programmer-1653351_440.webp';
+// import programmerImageSmallWebP from '../Assets/Images/programmer-1653351_640.webp';
+// import programmerImageMediumWebP from '../Assets/Images/programmer-1653351_1280.webp';
+// import programmerImageLargeWebP from '../Assets/Images/programmer-1653351.webp'
 
 export default function HeroImage() {
   return (
