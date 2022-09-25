33090e56dcb855433ca22a90b826442e2f422a47
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-25 15:15:02 -0400

    reduced project image sizes test

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,8 +1,8 @@
 import React, { createContext, useState, useEffect } from 'react';
-import WineRecommendationImage from '../Assets/Projects/wine-recommendations-square-version-2.jpg';
-import MoodyCreatureSociety from '../Assets/Projects/moody-creature-society.png';
-import PokemonIndex from '../Assets/Projects/pokemon-index.png';
-import DesignoMultiPageWebsite from '../Assets/Projects/designo-multi-page-website.png';
+import WineRecommendationImage from '../Assets/Projects/test/wine-recommendations-square-version-2.jpg';
+import MoodyCreatureSociety from '../Assets/Projects/test/moody-creature-society.jpg';
+import PokemonIndex from '../Assets/Projects/test/pokemon-index.jpg';
+import DesignoMultiPageWebsite from '../Assets/Projects/test/designo-multi-page-website.jpg';
 
 const PortfolioContext = createContext();
 
