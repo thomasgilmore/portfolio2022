46acd07cd1967f126e114de181be2cd1fd2e7116
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-09-27 00:37:58 -0400

    fixng resume button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,6 +7,7 @@ import NavBar from './NavBar';
 import Typewriter from 'typewriter-effect';
 import GitHubMark from '../Assets/GitHub-Mark/PNG/GitHub-Mark-120px-plus.png';
 import GitHubMarkLight from '../Assets/GitHub-Mark/PNG/GitHub-Mark-Light-120px-plus.png';
+import Resume from '../Assets/Resume.pdf';
 import LinkedinLogo from '../Assets/iconfinder_1_Linkedin_unofficial_colored_svg_5296501.png';
 import HeroImage from './HeroImage';
 import About from './About';
@@ -31,7 +32,7 @@ export default function Home() {
         }} />
         <div className='portfolioHeroSocialIcons'>
           <a href="https://github.com/thomasgilmore95" target="__blank">{theme === 'light' ? <img src={GitHubMark} className="gitHubLogo" width="50px" height="50px" alt="GitHub Logo Link to My GitHub Account" /> : <img src={GitHubMarkLight} className="gitHubLogo" width="50px" height="50px" alt="GitHub Logo Link to My HitHub Account" />}</a>
-          <a href="./Resume.pdf" target="__blank"><Button className={`${theme}ResumeButton`}>Resume</Button></a>
+          <a href={Resume} target="__blank"><Button className={`${theme}ResumeButton`}>Resume</Button></a>
           <a href="https://www.linkedin.com/in/thomasgilmore95/" target="__blank">
           <picture>
             <img src={LinkedinLogo} width="50px" height="50px" className="linkedinLogo" alt="Linkedin Logo to Linkedin Account" />
